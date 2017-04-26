18c7434cfdd530fa02a4e1ca7ed24472742219a7
Nasrin Ethiyil Abdul Jaleel <nasrinj@blueshark.ics.cs.cmu.edu>
2017-04-26 01:39:52 -0400

    correct multi-threaded ps impl with one worker

--- parameter_server.py
+++ parameter_server.py
@@ -8,7 +8,7 @@ import sys
 import threading
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5009
+TCP_PORT = 5010
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 MAX_NUMBER_WORKERS = 1
 current_worker_count = 0
@@ -22,10 +22,12 @@ global_avg = []
 #global_avg = np.array(global_avg)
 
 def add_local_gradients(local_gradients):
+    global global_sum
     for i,grad in enumerate(local_gradients):
         global_sum[i] += grad
 
 def average_gradients():
+    global global_avg
     global_avg = global_sum
     print("length of average is ,",len(global_avg))
     print(global_avg[9])
@@ -34,8 +36,12 @@ def average_gradients():
     print(global_avg[9])
 
 def zero_gradients():
+    global global_sum
+    print(global_avg[9])
     for i, grad in enumerate(global_sum):
-        global_sum[i] = global_sum[i].fill(ZERO)
+        global_sum[i].fill(0)
+    print(global_avg[9])
+    print(global_sum[9])
 
 def safeReceive(size,client_socket):
     received_size = 0
@@ -88,6 +94,7 @@ def handleClient(client_socket, client_address):
             average_gradients()
             ready_to_send = True
             zero_gradients()
+            print(global_avg[9])
 
     finally:
         lock_sum_gradients.release()
@@ -97,10 +104,11 @@ def handleClient(client_socket, client_address):
     while(ready_to_send == False):
         pass
 
-    client_socket.send(pickle.dumps(global_avg,pickle.HIGHEST_PROTOCOL))
+    send_data = pickle.dumps(global_avg,pickle.HIGHEST_PROTOCOL)
+    client_socket.send(send_data)
+    print("the size of sending data, ",sys.getsizeof(send_data))
     
-    data = pickle.dumps(global_avg,pickle.HIGHEST_PROTOCOL)
-    print("length is, ",len(pickle.loads(data)))
+    print("length is, ",len(pickle.loads(send_data)))
     lock_sum_gradients.acquire()
     print("lock acquired again")
     try:
